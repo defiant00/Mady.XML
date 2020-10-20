51ccb424b18a5b0ff78d2d90232a47843334501e
defiant00 <defiant00@gmail.com>
2020-10-20 16:54:42 -0400

    initial version

---
+++ Mady.XML/Manipulator.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace Mady.XML
+{
+	public class Manipulator
+	{
+		private XmlDocument Document { get; set; }
+		private int NextIndex { get; set; }
+
+		/// <summary>
+		/// Create a new XML Manipulator for the specified document.
+		/// </summary>
+		/// <param name="doc">The XML document to manipulate.</param>
+		public Manipulator(XmlDocument doc)
+		{
+			Document = doc;
+		}
+
+		/// <summary>
+		/// Manipulate the XML document, returning one or more XML documents.
+		/// </summary>
+		/// <param name="count">The number of documents to generate.</param>
+		/// <param name="tagsMatch">Tag/Attribute chains specifying the attributes to match on.</param>
+		/// <param name="valsInclude">Target values to include.</param>
+		/// <param name="valsExclude">Target values to exclude.</param>
+		/// <param name="tagsOnce">Tags to only include in one document.</param>
+		/// <param name="tagsExclude">Tags to exclude from all documents.</param>
+		/// <param name="emptyTag">Tag to create if the document is empty.</param>
+		/// <returns>List of XML documents resulting from the manipulation.</returns>
+		public List<XmlDocument> Manipulate(int count, IEnumerable<string> tagsMatch = null, IEnumerable<string> valsInclude = null, IEnumerable<string> valsExclude = null, IEnumerable<string> tagsOnce = null, IEnumerable<string> tagsExclude = null, string emptyTag = "empty")
+		{
+			var docs = new List<XmlDocument>(count);
+			var valueIndex = new Dictionary<string, int>();
+			var matchVals = tagsMatch?.Select(v => new MatchVal(v));
+			NextIndex = 0;
+
+			// Add commas to the end so they will match the search text.
+			tagsOnce = tagsOnce?.Select(v => v + ",");
+			tagsExclude = tagsExclude?.Select(v => v + ",");
+
+			for (int i = 0; i < count; i++)
+			{
+				docs.Add(new XmlDocument());
+			}
+
+			foreach (XmlNode n in Document)
+			{
+				List<NodeResult> results = ParseNode(n, docs, "", valueIndex, matchVals, valsInclude, valsExclude, tagsOnce, tagsExclude);
+				foreach (var nr in results)
+				{
+					docs[nr.Index].AppendChild(nr.Node);
+				}
+			}
+
+			foreach (var doc in docs)
+			{
+				bool hasBody = false;
+				foreach (XmlNode child in doc.ChildNodes)
+				{
+					if (child.NodeType == XmlNodeType.Element)
+					{
+						hasBody = true;
+					}
+				}
+				if (!hasBody)
+				{
+					doc.AppendChild(doc.CreateElement(emptyTag));
+				}
+			}
+
+			return docs;
+		}
+
+		private List<NodeResult> ParseNode(XmlNode node, List<XmlDocument> docs, string parentName, Dictionary<string, int> valueIndex, IEnumerable<MatchVal> matchVals,
+			IEnumerable<string> valsInclude, IEnumerable<string> valsExclude, IEnumerable<string> tagsOnce, IEnumerable<string> tagsExclude)
+		{
+			var result = new List<NodeResult>();
+
+			string name = parentName + node.Name + ",";
+
+			bool added = false;
+			string attribute = matchVals?.FirstOrDefault(v => v.Tags == name)?.Attribute;
+			if (attribute != null)
+			{
+				string val = node.Attributes[attribute]?.Value;
+				if (val != null && (valsInclude == null || valsInclude.Contains(val)) && (valsExclude == null || !valsExclude.Contains(val)))
+				{
+					int ind = NextIndex;
+					if (valueIndex.ContainsKey(val))
+					{
+						ind = valueIndex[val];
+					}
+					else
+					{
+						valueIndex[val] = NextIndex;
+						NextIndex = (NextIndex + 1) % docs.Count;
+					}
+
+					added = true;
+					result.Add(new NodeResult(docs, ind, node, true));
+				}
+			}
+
+			if (!added)
+			{
+				if (tagsOnce != null && tagsOnce.Contains(name))
+				{
+					int ind = NextIndex;
+					NextIndex = (NextIndex + 1) % docs.Count;
+					result.Add(new NodeResult(docs, ind, node, true));
+				}
+				else if (tagsExclude != null && tagsExclude.Contains(name))
+				{
+					// Do nothing if the node is on the exclude list.
+				}
+				else if (node.HasChildNodes &&
+					(matchVals?.Any(v => v.Tags.StartsWith(name)) == true ||
+					tagsOnce?.Any(v => v.StartsWith(name)) == true ||
+					tagsExclude?.Any(v => v.StartsWith(name)) == true))
+				{
+					var totRes = new List<NodeResult>();
+					foreach (XmlNode child in node.ChildNodes)
+					{
+						List<NodeResult> res = ParseNode(child, docs, name, valueIndex, matchVals, valsInclude, valsExclude, tagsOnce, tagsExclude);
+						totRes.AddRange(res);
+					}
+					var indices = totRes.Select(v => v.Index).Distinct();
+					foreach (int i in indices)
+					{
+						var nr = new NodeResult(docs, i, node, false);
+						var nodes = totRes.Where(v => v.Index == i);
+						foreach (var n in nodes)
+						{
+							nr.Node.AppendChild(n.Node);
+						}
+						result.Add(nr);
+					}
+				}
+				else
+				{
+					for (int i = 0; i < docs.Count; i++)
+					{
+						result.Add(new NodeResult(docs, i, node, true));
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private class NodeResult
+		{
+			public int Index { get; set; }
+			public XmlNode Node { get; set; }
+
+			public NodeResult(List<XmlDocument> docs, int index, XmlNode node, bool recursive)
+			{
+				Index = index;
+				Node = docs[index].ImportNode(node, recursive);
+			}
+		}
+
+		private class MatchVal
+		{
+			public string Tags { get; set; }
+			public string Attribute { get; set; }
+
+			public MatchVal(string val)
+			{
+				int ind = val.LastIndexOf(',') + 1;     // Add 1 so Tags includes the ',' and Attribute is just the name.
+				Tags = val.Substring(0, ind);
+				Attribute = val.Substring(ind);
+			}
+		}
+	}
+}
---
+++ Mady.XML/XmlHelper.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Xml;
+
+namespace Mady.XML
+{
+	[AttributeUsage(AttributeTargets.Property)]
+	public class XmlFormatAttribute : Attribute
+	{
+		public string FormatString { get; }
+		public XmlFormatAttribute(string formatString)
+		{
+			FormatString = formatString;
+		}
+	}
+
+	[AttributeUsage(AttributeTargets.Property)]
+	public class XmlExcludeAttribute : Attribute { }
+
+	public static class XmlHelper
+	{
+		/// <summary>
+		/// Creates a new XmlDocument instance with a default XML declaration.
+		/// </summary>
+		/// <returns>The new XmlDocument instance.</returns>
+		public static XmlDocument CreateDocument()
+		{
+			var doc = new XmlDocument();
+			doc.InsertBefore(doc.CreateXmlDeclaration("1.0", "UTF-8", ""), doc.DocumentElement);
+			return doc;
+		}
+
+		/// <summary>
+		/// Adds a child node to the current XmlNode.
+		/// </summary>
+		/// <param name="node">The node to add the node to.</param>
+		/// <param name="name">The name of the new node.</param>
+		/// <returns>The newly added XmlNode.</returns>
+		public static XmlNode Add(this XmlNode node, string name)
+		{
+			return node.AppendChild(node.OwnerDocument.CreateElement(name));
+		}
+
+		/// <summary>
+		/// Adds a node to the current XmlDocument.
+		/// </summary>
+		/// <param name="doc">The document to add the node to.</param>
+		/// <param name="name">The name of the new node.</param>
+		/// <returns>The newly added XmlNode.</returns>
+		public static XmlNode Add(this XmlDocument doc, string name)
+		{
+			return doc.AppendChild(doc.CreateElement(name));
+		}
+
+		/// <summary>
+		/// Sets the specified attribute on the current XmlNode.
+		/// </summary>
+		/// <param name="node">The node whose attribute will be set.</param>
+		/// <param name="name">The name of the attribute to set.</param>
+		/// <param name="value">The value to set for the attribute.</param>
+		/// <returns>The node whose attribute was set.</returns>
+		public static XmlNode Set(this XmlNode node, string name, object value)
+		{
+			var attribute = node.OwnerDocument.CreateAttribute(name);
+			attribute.Value = value?.ToString();
+			node.Attributes.SetNamedItem(attribute);
+			return node;
+		}
+
+		/// <summary>
+		/// Constructs an XML document based off of the current object and
+		/// all of its public properties.
+		/// </summary>
+		/// <param name="obj">The object to create XML for.</param>
+		/// <returns>The XML document based off of the specified object.</returns>
+		public static XmlDocument ToXml(this object obj)
+		{
+			var doc = CreateDocument();
+			doc.Add(obj.GetType().Name).AddPropertiesFromObject(obj);
+			return doc;
+		}
+
+		private static void AddPropertiesFromObject(this XmlNode parent, object obj)
+		{
+			foreach (var prop in obj.GetType().GetProperties())
+			{
+				object val = prop.GetValue(obj);
+				if (val != null && Attribute.GetCustomAttribute(prop, typeof(XmlExcludeAttribute)) == null)
+				{
+					var formatAttribute = (XmlFormatAttribute)Attribute.GetCustomAttribute(prop, typeof(XmlFormatAttribute));
+					if (formatAttribute != null)
+					{
+						var method = val.GetType().GetMethod("ToString", new[] { typeof(string) });
+						if (method != null)
+						{
+							val = method.Invoke(val, new[] { formatAttribute.FormatString });
+						}
+						else
+						{
+							throw new Exception($"Cannot apply format string \"{formatAttribute.FormatString}\" to type {prop.PropertyType.FullName}");
+						}
+					}
+
+					if (prop.PropertyType == typeof(string))
+					{
+						parent.Set(prop.Name, val);
+					}
+					else if (prop.PropertyType.GetInterface(nameof(IEnumerable)) != null)
+					{
+						foreach (object v in (IEnumerable)val)
+						{
+							parent.Add(v.GetType().Name).AddPropertiesFromObject(v);
+						}
+					}
+					else if (prop.PropertyType.IsClass)
+					{
+						parent.Add(prop.PropertyType.Name).AddPropertiesFromObject(val);
+					}
+					else
+					{
+						parent.Set(prop.Name, val);
+					}
+				}
+			}
+		}
+	}
+}
